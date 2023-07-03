194ed1eb2f7146fe3487b75d5801eeef109dc65a
Neville Brem <contact@nevillebrem.com>
2023-07-03 18:52:56 +0200

    implemented design for cart item

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -21,7 +21,7 @@ const Cart = ({ handleClose }) => {
         <Backdrop handleClose={handleClose}>
             <motion.div 
                 onClick={(e) => e.stopPropagation()}
-                className='w-[90%] h-[60%] bg-white p-10 m-auto rounded-2xl overflow-auto z-[1]'
+                className='w-[90%] h-fit max-h-[75%] bg-white p-10 m-auto rounded-2xl overflow-auto z-[1] max-w-6xl'
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
@@ -29,13 +29,14 @@ const Cart = ({ handleClose }) => {
             >
               <div className='flex justify-between'>
                 <h1 className='font-bold text-3xl'>Your Cart:</h1>
-                <FontAwesomeIcon icon={faXmark} onClick={handleClose} />
+                <FontAwesomeIcon className='cursor-pointer' icon={faXmark} onClick={handleClose} />
               </div>
-              <div className='flex flex-col gap-4 p-5'>
+              <div className='flex flex-col gap-4 my-4'>
                 <CartItem />
                 <CartItem />
                 <CartItem />
               </div>
+              <p>Subtotal: 9 CHF</p>
             </motion.div>
         </Backdrop>
     </div>
--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import {products} from '../../productList'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
+
 const CartItem = () => {
+
   return (
-    <div className='w-full bg-gray-400 border-gray-600 text-left rounded-xl py-4 px-2'>
-        Amogus
+    <div className='w-full border-gray-300 bg-gray-200 text-left rounded-xl py-4 px-4 max-w-5xl mx-auto'>
+      <div className='flex gap-4'>
+        <img src={products[5].img_main} alt={products[5].title} className='h-24 w-24 rounded-xl' />
+          <div className='flex justify-between my-auto'>
+            <div>
+              <h1 className='text-3xl font-bold text-left'>{products[5].title}</h1>
+              <p className='text-left font-semibold text-gray-600'>{products[5].price} CHF</p>
+            </div>
+            <FontAwesomeIcon icon={faTrashCan} className='text-2xl my-auto' />
+          </div>
+        </div>
     </div>
   )
 }
