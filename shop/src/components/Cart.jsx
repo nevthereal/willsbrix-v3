b953e921627676cdd1235c417cb6dc31a29dc108
Neville Brem <contact@nevillebrem.com>
2023-07-13 13:46:46 +0200

    cart && info updat

--- shop/src/components/Cart.jsx
+++ shop/src/components/Cart.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react'
 import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner, faXmark } from '@fortawesome/free-solid-svg-icons'
-import { faCcStripe } from '@fortawesome/free-brands-svg-icons'
 
 import Backdrop from './Backdrop'
 import CartItem from './CartItem'
@@ -82,7 +81,8 @@ const Cart = ({ handleClose }) => {
               {cart.items.length > 0 ? 
               <>
                 <p className='font-semibold text-sm'>Subtotal: {cart.getSubTotal().toFixed(2)} CHF</p>
-                <button className='border border-gray-400 py-1 px-2 rounded-lg hover:scale-105 duration-200 text-xl font-bold mt-2' onClick={checkout}>{isLoading ? <span className='cursor-wait'>Loading <FontAwesomeIcon icon={faSpinner} spin /></span> : <span>Checkout with <FontAwesomeIcon icon={faCcStripe} /></span>}</button>
+                <button className='border border-gray-400 py-1 px-2 rounded-lg hover:scale-105 duration-200 text-xl font-bold mt-2' onClick={checkout}>{isLoading ? <span className='cursor-wait'>Loading <FontAwesomeIcon icon={faSpinner} spin /></span> : <span>Checkout</span>}</button>
+                <p className='italic text-xs pt-1'>Have you read the <a href="/info">Info?</a></p>
               </> 
               :
               null}
