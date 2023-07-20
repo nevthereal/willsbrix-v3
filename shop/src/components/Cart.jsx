9a2d46e197cbc8f89286a9a1407f489a2fa8cb2d
Neville Brem <contact@nevillebrem.com>
2023-07-20 15:07:31 +0200

    little change

--- shop/src/components/Cart.jsx
+++ shop/src/components/Cart.jsx
@@ -1,29 +1,28 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFaceSmileWink, faSpinner, faXmark } from '@fortawesome/free-solid-svg-icons'
+import React, { useContext, useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-import Backdrop from './Backdrop'
-import CartItem from './CartItem'
+import Backdrop from "./Backdrop";
+import CartItem from "./CartItem";
 
-import { CartContext } from "../cartContext"
+import { CartContext } from "../cartContext";
 
 const fadeIn = {
   hidden: {
-    opacity: 0
-  }, visible: {
-    opacity: 1
-  }, exit: {
-    opacity: 0
-  }
-}
-
-
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
 
 const Cart = ({ handleClose }) => {
-
-  const cart = useContext(CartContext)
-  const [isLoading, setIsLoading] = useState(false)
+  const cart = useContext(CartContext);
+  const [isLoading, setIsLoading] = useState(false);
   const [showDelayedMessage, setShowDelayedMessage] = useState(false);
 
   useEffect(() => {
@@ -37,76 +36,99 @@ const Cart = ({ handleClose }) => {
     }
     return () => clearTimeout(timeoutId);
   }, [isLoading]);
-  
+
   const checkout = async () => {
     try {
-      setIsLoading(true)
-      const response = await fetch(`${import.meta.env.VITE_CHECKOUT_DOMAIN}/checkout`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ items: cart.items })
-      })
-  
-      const { url } = await response.json()
-  
+      setIsLoading(true);
+      const response = await fetch(
+        `${import.meta.env.VITE_CHECKOUT_DOMAIN}/checkout`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ items: cart.items }),
+        }
+      );
+
+      const { url } = await response.json();
+
       if (url) {
-        window.location.href = url
-        localStorage.removeItem('cartProducts');
+        window.location.href = url;
+        localStorage.removeItem("cartProducts");
       }
     } catch (error) {
-      alert("Couldn't connect to the server. Try again later")
+      alert("Couldn't connect to the server. Try again later");
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-
-  }
-  
+  };
 
   return (
     <div>
-        <Backdrop handleClose={handleClose}>
-            <motion.div 
-                onClick={(e) => e.stopPropagation()}
-                className='w-[90%] h-fit max-h-[75%] bg-white dark:bg-gray-800 px-4 py-10 m-auto rounded-2xl overflow-auto z-[1] max-w-6xl'
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                variants={fadeIn}
-            >
-              <div className='flex justify-between'>
-                <h1 className='font-bold text-4xl'>Your Cart:</h1>
-                <FontAwesomeIcon className='cursor-pointer text-xl' icon={faXmark} onClick={handleClose} />
-              </div>
-              <div className='flex flex-col gap-4 my-4'>
-                {cart.items.length === 0 ? 
-                <p className='text-xl'>There are no items in your cart. <a href="/products" className='font-bold'>Explore Products</a></p>
-                :
-                <>
+      <Backdrop handleClose={handleClose}>
+        <motion.div
+          onClick={(e) => e.stopPropagation()}
+          className='w-[90%] h-fit max-h-[75%] bg-white dark:bg-gray-800 px-4 py-10 m-auto rounded-2xl overflow-auto z-[1] max-w-6xl'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          variants={fadeIn}
+        >
+          <div className='flex justify-between'>
+            <h1 className='font-bold text-4xl'>Your Cart:</h1>
+            <FontAwesomeIcon
+              className='cursor-pointer text-xl'
+              icon={faXmark}
+              onClick={handleClose}
+            />
+          </div>
+          <div className='flex flex-col gap-4 my-4'>
+            {cart.items.length === 0 ? (
+              <p className='text-xl'>
+                There are no items in your cart.{" "}
+                <a href='/products' className='font-bold'>
+                  Explore Products
+                </a>
+              </p>
+            ) : (
+              <>
                 {cart.items.map((currentProduct, index) => (
                   <CartItem key={index} item={currentProduct} />
                 ))}
-                </>
-                }
-              </div>
-              
-              {cart.items.length > 0 ? 
-              <>
-                <p className='font-semibold text-sm'>Subtotal: {cart.getSubTotal().toFixed(2)} CHF</p>
-                <button className='border border-gray-400 py-1 px-2 rounded-lg hover:scale-105 duration-200 text-xl font-bold mt-2' onClick={checkout}>{isLoading ? <span className='cursor-wait'>Loading <FontAwesomeIcon icon={faSpinner} spin /></span> : <span>Checkout</span>}</button>
-              </> 
-              :
-              null}
-              {isLoading && showDelayedMessage && (
-              <p className='italic'>
-                Sometimes server requests can take a little longer. If so, be patient ...
-              </p>
-              )}
-            </motion.div>
-        </Backdrop>
-    </div>
-  )
-}
+              </>
+            )}
+          </div>
 
-export default Cart
+          {cart.items.length > 0 ? (
+            <>
+              <p className='font-semibold text-sm'>
+                Subtotal: {cart.getSubTotal().toFixed(2)} CHF
+              </p>
+              <button
+                className='border border-gray-400 py-1 px-2 rounded-lg hover:scale-105 duration-200 text-xl font-bold mt-2'
+                onClick={checkout}
+              >
+                {isLoading ? (
+                  <span className='cursor-wait'>
+                    Loading <FontAwesomeIcon icon={faSpinner} spin />
+                  </span>
+                ) : (
+                  <span>Checkout</span>
+                )}
+              </button>
+            </>
+          ) : null}
+          {isLoading && showDelayedMessage && (
+            <p className='italic'>
+              Sometimes server requests can take a little longer. If so, be
+              patient ...
+            </p>
+          )}
+        </motion.div>
+      </Backdrop>
+    </div>
+  );
+};
+
+export default Cart;
